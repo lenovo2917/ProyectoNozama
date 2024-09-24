bbb197ccf6562eeb243caf946f6a8d46cd4df354
lenovo2917 <94207895+lenovo2917@users.noreply.github.com>
2024-09-24 13:40:37 -0600

    Add files via upload

---
+++ ProyectoNozama/EJEMPLO.php
@@ -0,0 +1,9 @@
+<?php include './header.php'; ?>
+<main class="container"> 
+    <div class="row">
+        <div class="col">
+            <!-- AQUI COLOCAS TU CODIGO -->
+        </div>
+    </div>
+</main>
+<?php include './footer.php'; ?>
