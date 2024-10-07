4ee7ea6a224d6ce3e33fcb5eb198b05ebeec7bf0
S1LW0LF <ep3001.16@gmail.com>
2024-10-07 14:50:03 -0600

    Update

--- ProyectoNozama/EJEMPLO.php
+++ ProyectoNozama/EJEMPLO.php
@@ -2,7 +2,8 @@
 <main class="container"> 
     <div class="row">
         <div class="col">
-            <!-- AQUI COLOCAS TU CODIGO -->
+            <!-- AQUI COLOCAS TU CODIGO 1111111 -->
+             session_set_save_handler;
         </div>
     </div>
 </main>
