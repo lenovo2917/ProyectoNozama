a175bc6621f7273e045bc77b71d8b5d2ed34dec5
FranGuh <pedicles62900@gmail.com>
2024-10-09 21:12:22 -0600

    Sincronizar

---
+++ ProyectoNozama/pedido.php
@@ -0,0 +1,10 @@
+<?php include './header.php'; ?>
+<main class="container"> 
+    <div class="row">
+        <div class="col">
+            <!-- AQUI COLOCAS TU CODIGO gaaaaaa -->
+             
+        </div>
+    </div>
+</main>
+<?php include './footer.php'; ?>
